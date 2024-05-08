194b82eb919089cd5da067ab2a564362032deef3
tshepangkagiso <tshepangkagisomashigo8@gmail.com>
2024-05-08 17:16:11 +0200

    version 1

---
+++ Projects/projects.js
@@ -0,0 +1,28 @@
+Loader();
+DownloadCV();
+
+function Loader() {
+    window.addEventListener('DOMContentLoaded', (event) => {
+        document.getElementById("loading-overlay").style.display = "flex";
+        document.getElementById("content").style.display = "none";
+
+        setTimeout(function() {
+            document.getElementById("loading-overlay").style.display = "none";
+            document.getElementById("content").style.display = "block";
+        }, 2500);
+    });
+}
+
+
+function DownloadCV(){
+    document.getElementById('downloadButton').addEventListener('click', function() {
+
+        var downloadLink = document.createElement('a');
+        
+        downloadLink.href = '../resources/Tshepang Mashigo CV.pdf';
+        downloadLink.download = 'Tshepang Mashigo CV.pdf';
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+        document.body.removeChild(downloadLink);
+    });
+}
--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 DownloadCV();
 ProjectsRoute();
-
+Loader();
 
 
 function DownloadCV(){
@@ -8,8 +8,8 @@ function DownloadCV(){
 
         var downloadLink = document.createElement('a');
         
-        downloadLink.href = 'path/to/your/cv.pdf';
-        downloadLink.download = 'cv.pdf';
+        downloadLink.href = 'resources/Tshepang Mashigo CV.pdf';
+        downloadLink.download = 'Tshepang Mashigo CV.pdf';
         document.body.appendChild(downloadLink);
         downloadLink.click();
         document.body.removeChild(downloadLink);
@@ -25,6 +25,17 @@ function ProjectsRoute(){
         document.body.appendChild(downloadLink);
         downloadLink.click();
         document.body.removeChild(downloadLink);
+    });
+}
+function Loader() {
+    window.addEventListener('DOMContentLoaded', (event) => {
+        document.getElementById("loading-overlay").style.display = "flex";
+        document.getElementById("content").style.display = "none";
+
+        setTimeout(function() {
+            document.getElementById("loading-overlay").style.display = "none";
+            document.getElementById("content").style.display = "block";
+        }, 2500);
     });
 }
 
